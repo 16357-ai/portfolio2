def3228b0bbd8f4eb45194d8db68f4eadf2ee083
16357-ai <16357@phanthong.ac.th>
2026-07-20 16:06:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ document.getElementById("loader").style.display="none";
 },1800);
 
 });
+const toggle=document.getElementById("themeToggle");
+
+toggle.onclick=function(){
+
+document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+toggle.innerHTML="☀";
+}else{
+toggle.innerHTML="🌙";
+}
+
+}
